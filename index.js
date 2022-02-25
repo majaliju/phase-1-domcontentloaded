74a016f7ef242750e29231e941d2fc2e685885e3
majaliju <majalijunyc@gmail.com>
2022-02-25 15:38:04 -0500

    updated

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+  const textP = document.getElementById("text");
+  textP.innerText = "This is really cool!"
+  console.log(textP)
+});
